94ef362f60c32a9e8c264ec7d479e9e4513ee6c6
zeyad-hisham-cyper <zeyad20hisham@gmail.com>
2021-03-02 07:11:28 +0200

    first

---
+++ index.py
@@ -0,0 +1,30 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+import sys
+from os import path
+import os , time
+from os import *
+import pafy
+
+FORM_CLASS,_=loadUiType(path.join(path.dirname(__file__),"main.ui"))
+
+
+class main(QMainWindow,FORM_CLASS):
+    def __init__(self,parent=None):
+        super(main,self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+
+def main_app():
+    app = QApplication(sys.argv)
+    window = main()
+    window.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main_app()
+
