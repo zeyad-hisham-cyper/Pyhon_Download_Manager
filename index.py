057be5c70d2666fc469401b44ef3f91295a8c510
zeyad-hisham-cyper <zeyad20hisham@gmail.com>
2021-03-05 06:28:49 +0200

    File Done

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ from PyQt5.uic import loadUiType
 import sys
 from os import path
 import os , time
-from os import *
+import urllib.request
 
 
 FORM_CLASS,_=loadUiType(path.join(path.dirname(__file__),"main.ui"))
@@ -16,25 +16,52 @@ class main(QMainWindow,FORM_CLASS):
         super(main,self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.handel_UI()
+        self.handel_buttons()
+        
 
 
 
     def handel_UI(self):
         self.setWindowTitle("PyDM")
-        self.setFixedSize(737,468)
+        self.setFixedSize(737,285)
         pass
 
     def handel_buttons(self):
-        pass
+        self.Download1_5.clicked.connect(self.download) 
+        self.pushButton_3.clicked.connect(self.handel_Brows)
 
     def handel_Brows(self):
-        pass
+        save_loc = QFileDialog.getSaveFileName(self, caption="Save As", directory='.', filter="All Files (*,*)")
+        x = str(save_loc)
+        name = (x[2:].split(',')[0].replace("'", ''))
+        self.lineEdit_4.setText(name)
     
-    def handel_progress(self):
-        pass
+    def handel_progress(self, blocknum, blocksize, totalsize):
+        read = blocknum * blocksize
+
+        if totalsize >0:
+            percent = read*100 / totalsize
+            self.progressBar.setValue(percent)
+            QApplication.processEvents()
 
     def download(self):
-        pass
+        url = self.videoURL_2.text()
+        loc = self.lineEdit_4.text()
+
+        try:
+            urllib.request.urlretrieve(url, loc, self.handel_progress)
+        except Exception:
+            QMessageBox.warning(self, 'Alert', 'Download Faild' ) 
+            return 
+   
+        QMessageBox.information(self, 'Alert', 'Download Completed')
+        self.progressBar.setValue(0)
+        self.videoURL_2.setText("")
+        self.lineEdit_4.setText("")
+
+
+        
 
 
 def main_app():
